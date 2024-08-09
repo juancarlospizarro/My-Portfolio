0e9b081af6735c71552f61145911698b7debd796
Juan Carlos Pizarro Alonso <127835047+juancarlospizarro@users.noreply.github.com>
2024-08-09 09:52:02 +0200

    Proyectos

--- javascript/proyectos.js
+++ javascript/proyectos.js
@@ -1,22 +1,4 @@
-var imagen = document.getElementsByClassName('.imagen');
-
-        // Agrega un evento para escuchar cuando el cursor pasa sobre la imagen
-        imagen.addEventListener('mouseover', function() {
-            // Cambia el tamaño de la imagen cuando el cursor está sobre ella
-            imagen.style.transform = 'scale(1.1)';
-        });
-
-        // Agrega un evento para escuchar cuando el cursor sale de la imagen
-        imagen.addEventListener('mouseout', function() {
-            // Restaura el tamaño original de la imagen cuando el cursor sale
-            imagen.style.transform = 'scale(1)';
-        });
-
-
-
-
-
-var proyecto = document.querySelectorAll('.proyecto');
+var proyecto = document.querySelectorAll('.enlace_proyecto');
 
         // Define la función del manejador de eventos
         function mostrarAlerta() {
