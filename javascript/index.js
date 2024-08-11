ed0ad5007c0747952b17239736268ec180091641
Juan Carlos Pizarro Alonso <127835047+juancarlospizarro@users.noreply.github.com>
2024-08-11 11:10:25 +0200

    Cambios

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,17 @@
+// Selecciona todos los elementos con la clase 'enlace_proyecto'
+var proyecto = document.querySelectorAll('.enlace_proyecto');
 
+// Define la función del manejador de eventos
+function mostrarAlerta() {
+    alert('La página de información de este proyecto no está aún implementada.');
+}
+
+// Asigna el manejador de eventos a cada elemento
+proyecto.forEach(function(elemento) {
+    // Verifica si el atributo 'href' está vacío
+    if (elemento.getAttribute('href') === '') {
+        elemento.addEventListener('click', mostrarAlerta);
+    } 
+});
 
 
